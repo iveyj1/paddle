13b4d9d8f941ddc9f02bd8b4fb7255fff2f7d0cb
iveyj1 <iveyj@hotmail.com>
2017-08-19 21:47:23 -0400

    Remove unused functionality

--- esp32/paddle_esp/paddle_esp.ino
+++ esp32/paddle_esp/paddle_esp.ino
@@ -24,9 +24,6 @@ HardwareSerial Serial1(2);
 File logfile;
 
 int pin_red_led = 5;
-//int pin_grn_led = 8;
-//int pin_power_switch = 6;
-//int pin_keepalive = 12;
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
     Serial.printf("Listing directory: %s\n", dirname);
@@ -82,8 +79,7 @@ void setupTimer() {
     timerSemaphore = xSemaphoreCreateBinary();
     
     // Use 1st timer of 4 (counted from zero).
-    // Set 80 divider for prescaler (see ESP32 Technical Reference Manual for more
-    // info).
+    // Set 80 divider for prescaler (see ESP32 Technical Reference Manual).
     timer = timerBegin(0, 80, true);
     
     // Attach onTimer function to our timer.
@@ -98,22 +94,6 @@ void setupTimer() {
     
 }
 
-/*void checkShutdown()  {
-    if(digitalRead(pin_power_switch) == LOW){
-        if(logfile) {
-            logfile.close();
-            digitalWrite(pin_grn_led, HIGH);
-            delay(100);
-        }
-        delay(100);
-        digitalWrite(pin_keepalive, LOW);
-        while(1) {
-            ;
-        }
-    }
-    return;
-  }*/
-
 // blink out an error code
 void error(uint8_t errno) {
     Serial.println(String("Error number: ") + errno);
@@ -125,22 +105,18 @@ void error(uint8_t errno) {
             digitalWrite(pin_red_led, LOW);
             delay(450);
         }
-    //        for (i=errno; i<15; i++) {
         delay(2000);
-    //        }
-    //        checkShutdown();
     }
 }
 
 
 void openAcqFile() {
-    char testchar;
-    char filename[15];
+    char filename[20];
+    File file;
     //see if the card is present and can be initialized:
-    
     if (!SD.begin()) {
-    Serial.println("Card Mount Failed");
-    return;
+        Serial.println("Card Mount Failed");
+        return;
     }
     uint8_t cardType = SD.cardType();
     
@@ -149,120 +125,26 @@ void openAcqFile() {
         return;
     }
     
-    Serial.print("SD Card Type: ");
-    if (cardType == CARD_MMC) {
-        Serial.println("MMC");
-    } else if (cardType == CARD_SD) {
-        Serial.println("SDSC");
-    } else if (cardType == CARD_SDHC) {
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
-    
     uint64_t cardSize = SD.cardSize() / (1024 * 1024);
     Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-    Serial.printf("Reading file: %s\n", path);
-    File file = fs.open(path);
-    if(!file){
-        Serial.println("Failed to open file for reading");
-        return;
+  
+    strcpy(filename, "/ANALOG00.TXT");
+    for (uint8_t i = 0; i < 100; i++) {
+        filename[7] = '0' + i / 10;
+        filename[8] = '0' + i % 10;
+        // create if does not exist, do not open existing, write, sync after write
+        if (! SD.exists(filename)) {
+            break;
+        }
     }
-    
-    Serial.print("Read from file: ");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-    file.close();
-
-  strcpy(filename, "/ANALOG00.TXT");
-
-  /*    logfile = SD.open(filename, FILE_WRITE);
-      if( ! logfile ) {
-          Serial.println("could not open file");
-      }*/
-
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[7] = '0' + i / 10;
-    filename[8] = '0' + i % 10;
-    // create if does not exist, do not open existing, write, sync after write
-    if (! SD.exists(filename)) {
-      break;
-    }
-  }
-
-  if (SD.exists(filename)) {
-    if (!SD.remove(filename)) {
-      error(3);
-    }
-  }
-
-  logfile = SD.open(filename, FILE_WRITE);
-  if (!logfile) {
-    error(4);
-  }
-  //logfile.seek(0);
-  if (logfile.print("asdf") != 1) {
-    error(5);
-  }
-  logfile.flush();
-  logfile.close();
-
-    while(1);
-
-  logfile = SD.open(filename, FILE_WRITE);
-  if (!logfile) {
-    error(6);
-  }
-  logfile.seek(0);
-  testchar = 0;
-  testchar = logfile.read();
-  if (testchar != 't') {
-    error(7);
-  }
-  logfile.seek(0);
-  if (logfile.write('r') != 1) {
-    error(8);
-  }
-  logfile.close();
-
-
-  logfile = SD.open(filename, FILE_WRITE);
-  if (!logfile) {
-    error(9);
-  }
-  logfile.seek(0);
-  testchar = 0;
-  testchar = logfile.read();
-  if (testchar != 'r') {
-    error(10);
-  }
-  logfile.flush();
-  logfile.close();
-
-  if (SD.exists(filename)) {
-    if (!SD.remove(filename)) {
-      error(11);
-    }
-  }
-
-  logfile = SD.open(filename, FILE_WRITE);
-  if ( ! logfile ) {
-    error(12);
-  }
-  Serial.println("File tests ok");
+    file = SD.open(filename, FILE_WRITE);
+    if(!file)
+        Serial.printf("File %s not open", filename);
 }
 
 void setupPins() {
     pinMode(pin_red_led, OUTPUT);
     digitalWrite(pin_red_led, HIGH);
-  //    pinMode(pin_grn_led, OUTPUT);
-  //    digitalWrite(pin_grn_led, LOW);
-  //    pinMode(pin_power_switch, INPUT_PULLUP);
-  //    pinMode(pin_keepalive, OUTPUT);
-  //    digitalWrite(pin_keepalive, HIGH);
-
 }
 
 void setup() {
@@ -270,78 +152,26 @@ void setup() {
     Serial.begin(115200);
     setupPins();
     setupTimer();
-    openAcqFile();
-    //    analogReadResolution(12);
-    overrun = 0;
     listDir(SD, "/", 0);
-    Serial1.begin(9600);
-    Serial1.println("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"); // RMS message only
-    Serial1.println("$PMTK220,100*2F"); // 100ms position solutions
-    Serial1.println("$PMTK313,1*2E"); // enable SBAS
-
+    openAcqFile();
 }
 
 uint32_t i = 0, overrun_count = 0;
 
 uint32_t margin = 0;
 
-int16_t c;
-String s[2];
-uint8_t nema_index = 0;
-uint8_t nema_available = 0;
-uint8_t nema_available_last = 0;
-
 void loop() {
-    uint16_t adc0, adc1, adc2, adc3;
-    String outstr = "";
-    
+   
     digitalWrite(pin_red_led, LOW);
     if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE) {
-    overrun = 1;
-    //        digitalWrite(pin_grn_led, HIGH);
+        overrun = 1;
     }
     while (xSemaphoreTake(timerSemaphore, 0) != pdTRUE) {
-    margin++;
+        margin++;
     }
-    //dSerial.println(String("Margin: ")+margin);
     
     digitalWrite(pin_red_led, HIGH);
-    //    checkShutdown();
 
-
-    while ((c = Serial1.read()) != -1) {
-        if (char(c) == '$') {
-          //Serial.println(s[nema_index]);
-          for (int index1 = 0; index1 <= s[nema_index].length(); index1++) {
-              if (s[nema_index][index1] == '\r' || s[nema_index][index1] == '\n') {
-                  s[nema_index][index1] = ' ';
-              }
-          }
-          nema_available = nema_index;
-          nema_index = 1 - nema_index;
-          s[nema_index] = "";
-        } else {
-            s[nema_index] = s[nema_index] + char(c);
-        }
-    }
-    
-    String temp;
-    if (nema_available != nema_available_last) {
-    temp = s[nema_available];
-    nema_available_last = nema_available;
-    } else {
-        temp = "";
-    }
-    outstr = outstr + i + "," + adc0 + "," + adc1 + "," + adc2 + "," + adc3 + "," + overrun + "," + margin + "," + temp ;
-    logfile.println(outstr);
-    
-    //if (i % 256 == 0){
-        logfile.flush();
-    //}
-    i++;
-    overrun = 0;
-    margin = 0;
-    //    digitalWrite(pin_grn_led, LOW);
 
 }
 
