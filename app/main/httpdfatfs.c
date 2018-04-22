e4cea9a94a5291da303023f6e274f38fea52e392
iveyj1 <iveyj@hotmail.com>
2018-04-22 19:02:07 -0400

    Displaying times and sizes in dir

--- app/main/httpdfatfs.c
+++ app/main/httpdfatfs.c
@@ -16,12 +16,26 @@
 #include <dirent.h>
 #include <string.h>
 
+        
+#include <time.h>
+#define MAXPATH 256
+
 #define P_PATHBUFSIZE 256
 #include "sd.h"
 
 // Note: opendir() wants _no_ slash at the end of the path
 
 static const char *TAG = "httpdfatfs";
+
+int str_append(char *buff, size_t buffsize, const char *in)
+{
+    int buff_str_len = strlen(buff);
+    int in_str_len = strlen(in);
+    if((buff_str_len + in_str_len + 1) > buffsize) return -1;
+    memcpy(buff + buff_str_len, in, in_str_len);
+    *(buff + buff_str_len + in_str_len) = '\0';
+    return(buff_str_len + in_str_len);
+}
 
 CgiStatus ICACHE_FLASH_ATTR cgiFatFsDirHook(HttpdConnData *connData) {
     DIR *dp = connData->cgiData;
@@ -45,19 +59,14 @@ CgiStatus ICACHE_FLASH_ATTR cgiFatFsDirHook(HttpdConnData *connData) {
     }
 
     if (connData->url[strlen(connData->url)-1]=='/') needSlash=0; else needSlash=1;
+
+    path[0] = 0;
+    str_append(path, sizeof(path), SD_PREFIX);
+    str_append(path, sizeof(path), connData->url);  
     
+    //First call to this cgi. Open the dir so we can read it.
     if (dp == NULL) 
-    {
-        //First call to this cgi. Open the dir so we can read it.
-        int prefixlen = strlen(SD_PREFIX);
-        int urllen = strlen(connData->url);
-        if((prefixlen + urllen + 1) > P_PATHBUFSIZE) 
-        {
-            return HTTPD_CGI_NOTFOUND;
-        }
-        memcpy(path, SD_PREFIX, prefixlen);
-        memcpy(path + prefixlen, connData->url, urllen);
-        path[prefixlen + urllen] = '\0';
+    {     
         ESP_LOGI(TAG, "Opening directory %s", path);
         dp = opendir(path);
         if (!dp) 
@@ -65,97 +74,52 @@ CgiStatus ICACHE_FLASH_ATTR cgiFatFsDirHook(HttpdConnData *connData) {
             ESP_LOGI(TAG, "Can't open %s", path);
             return HTTPD_CGI_NOTFOUND;
         }
-        
-        
-        
 
-
-
-
-
-
-
-
-
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <pwd.h>
-#include <grp.h>
-#include <time.h>
-#include <locale.h>
-#include <langinfo.h>
-#include <stdio.h>
-#include <stdint.h>
-#define MAXPATH 255
-
-
-        struct dirent  *ep1;
-        struct stat     statbuf;
-        struct tm      *tm;
-        char            datestring[256];
-
-
-        int pathlen = strlen(path);
-        char filepath[MAXPATH] = "";
-        
-    /* Loop through directory entries. */
-        while ((ep1 = readdir(dp)) != NULL) 
-        {
-            /* Get entry's information. */
-            int namelen = strlen(ep1->d_name);
-            memcpy(filepath, path, pathlen);
-            filepath[pathlen] = '/';
-            memcpy(filepath + pathlen + 1, ep1->d_name, namelen);
-            filepath[pathlen + namelen + 1] = '\0'; // insufficient error checking
-            ESP_LOGI(TAG, "reading dir entry path: %s pathlen %d name %s namelen %d filepath %s", path, pathlen, ep1->d_name, namelen, filepath);
-
-            if (stat(filepath, &statbuf) == -1)
-            {
-                ESP_LOGI("TAG", "skipping %s", filepath);
-                continue;
-            }
-
-            /* Print size of file. */
-            ESP_LOGI(TAG, "Size: %9jd", (intmax_t)statbuf.st_size);
-
-
-            tm = localtime(&statbuf.st_mtime);
-
-            /* Get localized date string. */
-            ESP_LOGI(TAG, "strftime returns: %d", strftime(datestring, sizeof(datestring), "%Y-%m-%d_%H:%M:%S", tm));
-
-            ESP_LOGI(TAG, "date: %s\n", datestring);
-        }
-
-        closedir(dp);
-        dp = opendir(path);
-
-
-
-        
 
         connData->cgiData = dp;  // store directory info for next round
         httpdStartResponse(connData, 200);
         httpdHeader(connData, "Content-Type", httpdGetMimetype(connData->url));
         httpdHeader(connData, "Cache-Control", "max-age=3600, must-revalidate");
         httpdEndHeaders(connData);
-        sprintf(buff, "<html><head><title>Index of %s</title></head><body><h2>Index of %s</h2><ul>\n", connData->url, connData->url);
+        sprintf(buff, "<html><head><title>Index of %s</title><link rel=\"stylesheet\" type=\"text/css\" href=\"/html/style.css\"></head><body><h2>Index of %s</h2>\n", connData->url, connData->url);
+        httpdSend(connData, buff, -1);
+        sprintf(buff, "<table> <tr> <th>Filename</th> <th>Size</th> <th>Date</th></tr>\n");
         httpdSend(connData, buff, -1);
         return HTTPD_CGI_MORE;
     }
     else
     {
         ep = readdir(dp);
+
         if (ep && ep->d_name[0] != 0)  // there is another directory entry
         {
-            sprintf(buff, "<li><a href=\"%s%s%s\">%s</a></li>\n", connData->url, needSlash?"/":"", ep->d_name, ep->d_name);
-            httpdSend(connData, buff, -1);
+            char filepath[MAXPATH] = "";
+            str_append(filepath, sizeof(filepath), path);
+            str_append(filepath, sizeof(filepath), "/");
+            str_append(filepath, sizeof(filepath), ep->d_name);   
+            
+            ESP_LOGI(TAG, "reading dir entry path: %s name: %s filepath: %s", path, ep->d_name, filepath);
+            
+            struct stat     statbuf;
+            struct tm      *tm;
+            char            datestring[256];         
+            if (stat(filepath, &statbuf) != -1)
+            {
+                ESP_LOGI(TAG, "Size: %9jd", (intmax_t)statbuf.st_size);
+                tm = localtime(&statbuf.st_mtime);
+                ESP_LOGI(TAG, "strftime returns: %d", strftime(datestring, sizeof(datestring), "%Y-%m-%d_%H:%M:%S", tm));
+                ESP_LOGI(TAG, "date: %s\n", datestring);
+                sprintf(buff, "<tr><td><a href=\"%s%s%s\">%s</a></td><td>%lld</td><td>%s</td></tr>\n", connData->url, needSlash?"/":"", ep->d_name, ep->d_name, (intmax_t)statbuf.st_size, datestring);
+                httpdSend(connData, buff, -1);
+            }
+            else
+            {
+                ESP_LOGI("TAG", "skipping %s", filepath);
+            }
             return HTTPD_CGI_MORE;
         } else {
             closedir(dp);
-            sprintf(buff, "</ul></body></html>");
+            sprintf(buff, "</table></body></html>");
             httpdSend(connData, buff, -1);
         }
     }
